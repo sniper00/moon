d35187271571e34c96491aa9176c1dea0da280bf
sniper00 <hanyongtao@live.com>
2018-06-09 14:44:21 +0800

    commit

--- example/call_example_sender.lua
+++ example/call_example_sender.lua
@@ -9,17 +9,18 @@ moon.start(function()
         file = "call_example_receiver.lua"
     })
 
+
     moon.start_coroutine(function()
         print(moon.co_call('lua',receiverid,"SUB",1000,2000))
-
         moon.co_wait(1000)
 
         print(moon.co_call('lua',receiverid,"ACCUM",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
 
-        -- will return ,service exit/crash
+        moon.co_wait(5000)
         print(moon.co_call('lua',receiverid,"ADD",100,99))
-        -- call exited service
-        print(moon.co_call('lua',receiverid,"ADD",100,99))
+
+        moon.co_wait(5000)
+        print(moon.co_call('lua',receiverid,"SUB",100,99))
     end)
 
 end)
