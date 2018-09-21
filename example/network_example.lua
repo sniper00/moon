5ba9892d008e07d460ba9908cf04a4647bc5540c
sniper00 <hanyongtao@live.com>
2018-09-21 22:56:57 +0800

    修改网络模块

--- example/network_example.lua
+++ example/network_example.lua
@@ -1,64 +1,21 @@
 local moon = require("moon")
+local tcpserver = require("moon.tcpserver")
 
---Echo Server 示例
+--Echo Server Example
 
-local network = moon.get_tcp("network")
-local socket_handler = {}
-
--- enum class socket_data_type :std::uint8_t
--- {
---     socket_connect = 1,
---     socket_accept =2,
---     socket_recv = 3,
---     socket_close =4,
---     socket_error = 5,
---     socket_logic_error = 6
--- };
-
-socket_handler[1] = function(sessionid, data)
-	print("connect ",sessionid,data)
-end
-
-socket_handler[2] = function(sessionid, data)
-	print("accept ",sessionid,data)
-end
-
-local response = string.pack("H",2)
-
-socket_handler[3] = function(sessionid, data)
-	network:send(sessionid, response..data)
-	--print("recv", data)
-end
-
-socket_handler[4] = function(sessionid, data)
-	print("close ",sessionid, data)
-end
-
-socket_handler[5] = function(sessionid, data)
-	print("error ",sessionid, data)
-end
-
-socket_handler[6] = function(sessionid, data)
-	print("logic error ",sessionid, data)
-end
-
-moon.start(function()
-
-	moon.register_protocol(
-	{
-		name = "socket",
-		PTYPE = moon.PSOCKET,
-		pack = function(...) return ... end,
-		dispatch = function(msg)
-			local sessionid = msg:sender()
-			local subtype = msg:subtype()
-			local f = socket_handler[subtype]
-			if f then
-				local data = msg:bytes()
-				f(sessionid, data)
-			end
-		end
-	})
+tcpserver.on("accept",function(sessionid, msg)
+    print("accept ", sessionid, msg:bytes())
 end)
 
+local response = string.pack("H", 2)
+tcpserver.on("message",function(sessionid, msg)
+    tcpserver.send(sessionid, response .. msg:bytes())
+end)
 
+tcpserver.on("close",function(sessionid, msg)
+    print("close ", sessionid, msg:bytes())
+end)
+
+tcpserver.on("error",function(sessionid, msg)
+    print("error ", sessionid, msg:bytes())
+end)
