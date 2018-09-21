a868fb54c0d981194798d4449e1f2b4dbccc85fb
sniper00 <hanyongtao@live.com>
2018-09-21 23:52:10 +0800

    修改LUA C/C++ 模块定义规则

--- premake5.lua
+++ premake5.lua
@@ -111,27 +111,27 @@ project "moon"
 
 
 --[[
-    lua 第三方模块
+    lua C/C++模块
+    @dir： 模块源文件所在路径，相对于当前目录的路径
     @name: LUAMOD name
     @normaladdon : 平台通用的附加项
     @winddowsaddon : windows下的附加项
     @linuxaddon : linux下的附加项
 
     使用：
-    把lua模块源码文件夹放在 third 目录下，确保name和文件夹名字一致
-    导出符号：先定义LUA_LIB 使用 LUAMOD_API 导出符号
+    模块编写规范：使用 LUAMOD_API 导出符号(windows)
 
     注意：
     默认使用C编译器编译，可以使用 *addon 参数进行更改
 ]]
-local function add_third_lua_module( name,normaladdon,winddowsaddon,linuxaddon)
+local function add_lua_module(dir, name, normaladdon, winddowsaddon, linuxaddon )
     project(name)
     objdir ("obj/"..name.."/%{cfg.platform}_%{cfg.buildcfg}") --编译生成的中间文件目录
     location ("build/"..name) -- 生成的工程文件目录
     kind "SharedLib" -- 静态库 StaticLib， 动态库 SharedLib
     targetdir "bin/%{cfg.buildcfg}" --目标文件目录
     includedirs {"./third/lua53","./third"} --头文件搜索目录
-    files { "./third/"..name.."/**.h", "./third/"..name.."/**.c"} --需要编译的文件， **.c 递归搜索匹配的文件
+    files { dir.."/**.h",dir.."/**.hpp", dir.."/**.c",dir.."/**.cpp"} --需要编译的文件， **.c 递归搜索匹配的文件
     targetprefix "" -- linux 下需要去掉动态库 'lib' 前缀
     language "C"
     postbuildcommands{"{COPY} %{wks.location}/bin/%{cfg.buildcfg}/%{cfg.buildtarget.name} %{wks.location}/example/clib/"} -- 编译完后拷贝到example目录
@@ -140,48 +140,7 @@ local function add_third_lua_module( name,normaladdon,winddowsaddon,linuxaddon)
     end
     filter { "system:windows" }
         links{"lua53"} -- windows 版需要链接 lua 库
-        defines {"LUA_BUILD_AS_DLL"} -- windows下动态库导出宏定义
-        if type(winddowsaddon)=="function" then
-            winddowsaddon()
-        end
-    filter { "system:linux" }
-        if type(linuxaddon)=="function" then
-            linuxaddon()
-        end
-end
-
-
---[[
-    自己编写的lua模块
-    @name: LUAMOD name
-    @normaladdon : 平台通用的附加项
-    @winddowsaddon : windows下的附加项
-    @linuxaddon : linux下的附加项
-
-    使用：
-    把lua模块源码文件夹放在当前目录的 lualib 目录下，确保name和文件夹名字一致
-    导出符号：先定义LUA_LIB 使用 LUAMOD_API 导出符号
-
-    注意：
-    默认使用C++编译器编译，可以使用 *addon 参数进行更改
-]]
-local function add_lua_module( name,normaladdon,winddowsaddon,linuxaddon)
-    project(name)
-    objdir ("obj/"..name.."/%{cfg.platform}_%{cfg.buildcfg}") --编译生成的中间文件目录
-    location ("build/"..name) -- 生成的工程文件目录
-    kind "SharedLib" -- 静态库 StaticLib， 动态库 SharedLib
-    targetdir "bin/%{cfg.buildcfg}" --目标文件目录
-    includedirs {"./third/lua53","./third"} --头文件搜索目录
-    files { "./lualib/"..name.."/**.h","./lualib/"..name.."/**.hpp", "./lualib/"..name.."/**.cpp"} --需要编译的文件
-    targetprefix "" -- linux 下需要去掉动态库 'lib' 前缀
-    language "C++"
-    postbuildcommands{"{COPY} %{wks.location}/bin/%{cfg.buildcfg}/%{cfg.buildtarget.name} %{wks.location}/example/clib/"} -- 编译完后拷贝到example目录
-    if type(normaladdon)=="function" then
-        normaladdon()
-    end
-    filter { "system:windows" }
-        links{"lua53"} -- windows 版需要链接 lua 库
-        defines {"LUA_BUILD_AS_DLL"} -- windows下动态库导出宏定义
+        defines {"LUA_BUILD_AS_DLL","LUA_LIB"} -- windows下动态库导出宏定义
         if type(winddowsaddon)=="function" then
             winddowsaddon()
         end
@@ -197,8 +156,8 @@ end
 ]]
 
 -------------------------protobuf--------------------
-add_third_lua_module("protobuf",nil,
-function ( ... )
+add_lua_module("./third/protobuf", "protobuf",nil,
+function()
     language "C++"
     buildoptions {"/TP"} -- windows 下强制用C++编译，默认会根据文件后缀名选择编译
 end)
@@ -211,24 +170,24 @@ end)
 ]]
 
 -- ---------------------mysql-----------------------
--- add_lua_module("mysql",
--- function( ... )
+-- add_lua_module("./lualib/mysql","mysql",
+-- function()
 --     language "C++"
 -- end,
--- function ( ... )
+-- function ()
 --     if os.istarget("windows") then
 --         assert(os.getenv("MYSQL_HOME"),"please set mysql environment 'MYSQL_HOME'")
 --         includedirs {os.getenv("MYSQL_HOME").. "/include"}
---         libdirs{os.getenv("MYSQL_HOME").. "/lib"} -- 搜索目录
+--         libdirs{os.getenv("MYSQL_HOME").. "/lib"} -- mysql C client库搜索目录
 --         links{"libmysql"}
 --     end
 -- end,
--- function ( ... )
+-- function ()
 --     if os.istarget("linux") then
 --         assert(os.isdir("/usr/include/mysql"),"please make sure you have install mysql, or modify the default include path,'/usr/include/mysql'")
 --         assert(os.isdir("/usr/lib64/mysql"),"please make sure you have install mysql, or modify the default lib path,'/usr/lib64/mysql'")
 --         includedirs {"/usr/include/mysql"}
---         libdirs{"/usr/lib64/mysql"} -- 搜索目录
+--         libdirs{"/usr/lib64/mysql"} -- mysql C client库搜索目录
 --         links{"mysqlclient"}
 --     end
 -- end
