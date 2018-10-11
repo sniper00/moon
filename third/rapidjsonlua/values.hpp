7af0e0ee5a42e731c2ae8484ee5e2fc0715f1f9c
sniper00 <hanyongtao@live.com>
2018-10-11 13:10:44 +0800

    fix warning

--- third/rapidjsonlua/values.hpp
+++ third/rapidjsonlua/values.hpp
@@ -148,7 +148,7 @@ namespace values {
 			return true;
 		}
 		bool EndArray(rapidjson::SizeType elementCount) {
-			assert(elementCount == context_.index_);
+			assert(static_cast<int>(elementCount) == context_.index_);
 			context_ = stack_.back();
 			stack_.pop_back();
 			context_.submit(L);
