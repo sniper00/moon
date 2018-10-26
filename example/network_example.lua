c80ce6bfb7188c9f74ab463524e2c73e9920d5c7
sniper00 <hanyongtao@live.com>
2018-10-26 16:26:46 +0800

    修改 tcp network, 同一个服务：区分默认协议和 websocket 协议

--- example/network_example.lua
+++ example/network_example.lua
@@ -1,21 +1,47 @@
 local moon = require("moon")
-local tcpserver = require("moon.tcpserver")
-
 --Echo Server Example
 
-tcpserver.on("accept",function(sessionid, msg)
-    print("accept ", sessionid, msg:bytes())
-end)
+do
+    -------------------2 bytes len (big endian) protocol------------------------
+    local tcpserver = require("moon.tcpserver")
 
-local response = string.pack("H", 2)
-tcpserver.on("message",function(sessionid, msg)
-    tcpserver.send(sessionid, response .. msg:bytes())
-end)
+    tcpserver.on("accept",function(sessionid, msg)
+        print("accept ", sessionid, msg:bytes())
+    end)
 
-tcpserver.on("close",function(sessionid, msg)
-    print("close ", sessionid, msg:bytes())
-end)
+    tcpserver.on("message",function(sessionid, msg)
+        --tcpserver.send(sessionid, msg:bytes())
+        tcpserver.send_message(sessionid, msg)
+    end)
 
-tcpserver.on("error",function(sessionid, msg)
-    print("error ", sessionid, msg:bytes())
-end)
+    tcpserver.on("close",function(sessionid, msg)
+        print("close ", sessionid, msg:bytes())
+    end)
+
+    tcpserver.on("error",function(sessionid, msg)
+        print("error ", sessionid, msg:bytes())
+    end)
+end
+
+do
+-------------------WEBSOCKET------------------------
+    local websocket = require("moon.websocket")
+
+    websocket.on("accept",function(sessionid, msg)
+        print("wsaccept ", sessionid, msg:bytes())
+    end)
+
+    websocket.on("message",function(sessionid, msg)
+        --websocket.send(sessionid, msg:bytes())
+        websocket.send_message(sessionid, msg)
+    end)
+
+    websocket.on("close",function(sessionid, msg)
+        print("wsclose ", sessionid, msg:bytes())
+    end)
+
+    websocket.on("error",function(sessionid, msg)
+        print("wserror ", sessionid, msg:bytes())
+    end)
+end
+
