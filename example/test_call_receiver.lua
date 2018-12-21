097a6c7a861015af9a320124e2d352bbe827a653
sniper00 <hanyongtao@live.com>
2018-12-21 19:34:39 +0800

    优化定时器调用；增加测试；

---
+++ example/test_call_receiver.lua
@@ -0,0 +1,45 @@
+local moon   = require("moon")
+
+local command = {}
+
+command.ADD =  function(a,b)
+    assert(false)
+    return a+b
+end
+
+command.SUB = function(a,b)
+    return a-b
+end
+
+command.MUL = function(a,b)
+    return a*b
+end
+
+command.ACCUM = function(...)
+    local numbers = {...}
+    local total = 0
+    for _,v in pairs(numbers) do
+        total = total + v
+    end
+    return total
+end
+
+command.EXIT = function()
+    moon.removeself()
+end
+
+local function docmd(sender,responseid, CMD,...)
+    local f = command[CMD]
+    if f then
+        moon.response('lua',sender,responseid,f(...))
+    else
+        error(string.format("Unknown command %s", tostring(CMD)))
+    end
+end
+
+moon.dispatch('lua',function(msg,p)
+    local sender = msg:sender()
+    local responseid = msg:responseid()
+    docmd(sender,responseid, p.unpack(msg))
+end)
+
