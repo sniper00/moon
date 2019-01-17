82fb5ff8176a1445a0b3fb6e3782f744f7168693
sniper00 <hanyongtao@live.com>
2019-01-17 13:18:31 +0800

    update aoi library

--- lualib-src/laoi/quad_tree.hpp
+++ lualib-src/laoi/quad_tree.hpp
@@ -11,9 +11,9 @@ namespace math
 
     using node_rect = math::rect;
 
-	class quad_tree_node
-	{
-	public:
+    class quad_tree_node
+    {
+    public:
 
         struct quad_tree_object
         {
@@ -22,12 +22,12 @@ namespace math
             objectid_t uid;
         };
 
-		// Construct a quadtree node with the given bounds 
-		explicit quad_tree_node(quad_tree_node* parent, const node_rect& bounds)
-			:parent_(parent)
-            ,bounds_(bounds)
-		{
-		}
+        // Construct a quadtree node with the given bounds 
+        explicit quad_tree_node(quad_tree_node* parent, const node_rect& bounds)
+            :parent_(parent)
+            , bounds_(bounds)
+        {
+        }
 
         quad_tree_node(const quad_tree_node&) = delete;
         quad_tree_node& operator=(const quad_tree_node&) = delete;
@@ -35,102 +35,102 @@ namespace math
         quad_tree_node(quad_tree_node&&) = default;
         quad_tree_node& operator=(quad_tree_node&&) = default;
 
-		bool empty() const
-		{
-			return (bounds_.empty() || (contents_.empty() && nodes_.empty()));
-		}
+        bool empty() const
+        {
+            return (bounds_.empty() || (contents_.empty() && nodes_.empty()));
+        }
 
-		const auto& contents() const
-		{
-			return contents_;
-		}
+        const auto& contents() const
+        {
+            return contents_;
+        }
 
-		const node_rect& bounds() const
-		{
-			return bounds_;
-		}
+        const node_rect& bounds() const
+        {
+            return bounds_;
+        }
 
-		size_t count() const
-		{
-			size_t n = 0;
-			for (auto& node : nodes_)
-			{
-				n += node.count();
-			}
+        size_t count() const
+        {
+            size_t n = 0;
+            for (auto& node : nodes_)
+            {
+                n += node.count();
+            }
 
-			n += contents_.size();
-			return n;
-		}
+            n += contents_.size();
+            return n;
+        }
 
-		void subtree_contents(std::vector<objectid_t>& ret) const
-		{
-			for (auto& node : nodes_)
-			{
-				node.subtree_contents(ret);
-			}
+        void subtree_contents(std::vector<objectid_t>& ret) const
+        {
+            for (auto& node : nodes_)
+            {
+                node.subtree_contents(ret);
+            }
 
-			for (auto& item : contents_)
-			{
-				ret.push_back(item.first);
-			}
-		}
+            for (auto& item : contents_)
+            {
+                ret.push_back(item.first);
+            }
+        }
 
-		void query(const node_rect& queryArea, std::vector<objectid_t>& ret) const
-		{
-			for (auto& item : contents_)
-			{
-				if (queryArea.contains(item.second.x, item.second.y))
-				{
+        void query(const node_rect& queryArea, std::vector<objectid_t>& ret) const
+        {
+            for (auto& item : contents_)
+            {
+                if (queryArea.contains(item.second.x, item.second.y))
+                {
                     ret.push_back(item.first);
-				}
-			}
+                }
+            }
 
-			for (auto& node : nodes_)
-			{
-				if (node.empty())
-				{
-					continue;
-				}
+            for (auto& node : nodes_)
+            {
+                if (node.empty())
+                {
+                    continue;
+                }
 
-				// Case 1: search area completely contained by sub-quad
-				// if a node completely contains the query area, go down that branch
-				// and skip the remaining nodes (break this loop)
-				if (node.bounds().contains(queryArea))
-				{
-					node.query(queryArea,ret);
-					break;
-				}
+                // Case 1: search area completely contained by sub-quad
+                // if a node completely contains the query area, go down that branch
+                // and skip the remaining nodes (break this loop)
+                if (node.bounds().contains(queryArea))
+                {
+                    node.query(queryArea, ret);
+                    break;
+                }
 
-				// Case 2: Sub-quad completely contained by search area 
-				// if the query area completely contains a sub-quad,
-				// just add all the contents of that quad and it's children 
-				// to the result set. You need to continue the loop to test 
-				// the other quads
-				if (queryArea.contains(node.bounds()))
-				{
-					node.subtree_contents(ret);
-					continue;
-				}
+                // Case 2: Sub-quad completely contained by search area 
+                // if the query area completely contains a sub-quad,
+                // just add all the contents of that quad and it's children 
+                // to the result set. You need to continue the loop to test 
+                // the other quads
+                if (queryArea.contains(node.bounds()))
+                {
+                    node.subtree_contents(ret);
+                    continue;
+                }
 
-				// Case 3: search area intersects with sub-quad
-				// traverse into this quad, continue the loop to search other
-				// quads
+                // Case 3: search area intersects with sub-quad
+                // traverse into this quad, continue the loop to search other
+                // quads
 
-				if (node.bounds().intersects(queryArea))
-				{
-					node.query(queryArea, ret);
-				}
-			}
-		}
+                if (node.bounds().intersects(queryArea))
+                {
+                    node.query(queryArea, ret);
+                }
+            }
+        }
 
         quad_tree_node* insert(objectid_t uid, float x, float y)
-		{
-			// if the item is not contained in this quad, there's a problem
-			if (!bounds_.contains(x,y))
-			{
-				assert(false && "feature is out of the bounds of this quadtree node");
-				return nullptr;
-			}
+        {
+            // if the item is not contained in this quad, there's a problem
+            if (!bounds_.contains(x, y))
+            {
+                assert(false && "feature is out of the bounds of this quadtree node");
+                return nullptr;
+            }
 
             auto iter = contents_.find(uid);
             if (iter != contents_.end())
@@ -140,51 +140,51 @@ namespace math
                 return this;
             }
 
-			// if the subnodes are null create them. may not be sucessfull: see below
-			// we may be at the smallest allowed size in which case the subnodes will not be created
-			if (nodes_.size() == 0)
-			{
-				create_sub_nodes();
-			}
+            // if the subnodes are null create them. may not be sucessfull: see below
+            // we may be at the smallest allowed size in which case the subnodes will not be created
+            if (nodes_.size() == 0)
+            {
+                create_sub_nodes();
+            }
 
-			// for each subnode:
-			// if the node contains the item, add the item to that node and return
-			// this recurses into the node that is just large enough to fit this item
-			for (auto& node : nodes_)
-			{
-				if(node.bounds().contains(x,y))
-				{
-					return node.insert(uid,x,y);
-				}
-			}
+            // for each subnode:
+            // if the node contains the item, add the item to that node and return
+            // this recurses into the node that is just large enough to fit this item
+            for (auto& node : nodes_)
+            {
+                if (node.bounds().contains(x, y))
+                {
+                    return node.insert(uid, x, y);
+                }
+            }
 
-			// if we make it to here, either
-			// 1) none of the subnodes completely contained the item. or
-			// 2) we're at the smallest subnode size allowed 
-			// add the item to this node's contents.
+            // if we make it to here, either
+            // 1) none of the subnodes completely contained the item. or
+            // 2) we're at the smallest subnode size allowed 
+            // add the item to this node's contents.
             contents_.emplace(uid, quad_tree_object{ x,y,uid });
-			return this;
-		}
+            return this;
+        }
 
-		template<typename Action>
-		void foreach(Action&& f) const
-		{
-			f(this);
+        template<typename Action>
+        void foreach(Action&& f) const
+        {
+            f(this);
 
-			for (auto& node : nodes_)
-			{
-				node.foreach(std::forward<Action>(f));
-			}
-		}
+            for (auto& node : nodes_)
+            {
+                node.foreach(std::forward<Action>(f));
+            }
+        }
 
-		void clear()
-		{
-			contents_.clear();
-			for (auto& node : nodes_)
-			{
-				node.clear();
-			}
-		}
+        void clear()
+        {
+            contents_.clear();
+            for (auto& node : nodes_)
+            {
+                node.clear();
+            }
+        }
 
         void erase(objectid_t uid)
         {
@@ -205,60 +205,60 @@ namespace math
             return parent_;
         }
 
-	private:
-		void create_sub_nodes()
-		{
-			// the smallest subnode has an area 
-			if ((bounds_.width*bounds_.height) <= aoi_radius* aoi_radius)
-				return;
+    private:
+        void create_sub_nodes()
+        {
+            // the smallest subnode has an area 
+            if ((bounds_.width*bounds_.height) <= aoi_radius * aoi_radius)
+                return;
 
             float halfw = bounds_.width / 2;
             float halfh = bounds_.height / 2;
 
-			// left top node
-			nodes_.emplace_back(this, node_rect(bounds_.x, bounds_.midy(), halfw, halfh));
-			// left bottom node
-			nodes_.emplace_back(this, node_rect(bounds_.x, bounds_.y, halfw, halfh));
-			// right top node
-			nodes_.emplace_back(this, node_rect(bounds_.midx(), bounds_.midy(), halfw, halfh));
-			// right bottom node
-			nodes_.emplace_back(this, node_rect(bounds_.midx(), bounds_.y, halfw, halfh));
-		}
+            // left top node
+            nodes_.emplace_back(this, node_rect(bounds_.x, bounds_.midy(), halfw, halfh));
+            // left bottom node
+            nodes_.emplace_back(this, node_rect(bounds_.x, bounds_.y, halfw, halfh));
+            // right top node
+            nodes_.emplace_back(this, node_rect(bounds_.midx(), bounds_.midy(), halfw, halfh));
+            // right bottom node
+            nodes_.emplace_back(this, node_rect(bounds_.midx(), bounds_.y, halfw, halfh));
+        }
 
-	private:
+    private:
         quad_tree_node* parent_;
-		// The area of this node
-		node_rect bounds_;
-		// The child nodes of the QuadTree
-		std::vector<quad_tree_node> nodes_;
+        // The area of this node
+        node_rect bounds_;
+        // The child nodes of the QuadTree
+        std::vector<quad_tree_node> nodes_;
         // The objectss of the QuadTree
         std::unordered_map<objectid_t, quad_tree_object> contents_;
-	};
+    };
 
-	class quad_tree
-	{
-	public:
-		explicit quad_tree(const node_rect& rc)
-			:rect_(rc)
+    class quad_tree
+    {
+    public:
+        explicit quad_tree(const node_rect& rc)
+            :rect_(rc)
             , root_(nullptr, rc)
-		{
+        {
 
-		}
+        }
 
         quad_tree(const quad_tree&) = delete;
         quad_tree& operator=(const quad_tree&) = delete;
 
-		size_t count()
-		{
-			return root_.count();
-		}
+        size_t count()
+        {
+            return root_.count();
+        }
 
-		void insert(objectid_t uid, float x, float y)
-		{
-            auto node = root_.insert(uid,x,y);
+        void insert(objectid_t uid, float x, float y)
+        {
+            auto node = root_.insert(uid, x, y);
             assert(nullptr != node);
             items_.try_emplace(uid, node);
-		}
+        }
 
         void update(objectid_t uid, float x, float y)
         {
@@ -296,8 +296,8 @@ namespace math
             insert(uid, x, y);
         }
 
-		void query(objectid_t uid, float width, float height, std::vector<objectid_t>& out)
-		{
+        void query(objectid_t uid, float width, float height, std::vector<objectid_t>& out)
+        {
             auto iter = items_.find(uid);
             if (iter != items_.end())
             {
@@ -324,28 +324,28 @@ namespace math
                 } while (true);
                 root_.query(rc, out);
             }
-		}
+        }
 
         void query(const node_rect& rc, std::vector<objectid_t>& out)
         {
             root_.query(rc, out);
         }
 
-		template<typename Action>
-		void foreach(Action&& f)
-		{
-			root_.foreach(std::forward<Action>(f));
-		}
+        template<typename Action>
+        void foreach(Action&& f)
+        {
+            root_.foreach(std::forward<Action>(f));
+        }
 
-		const node_rect& rect() const
-		{
-			return rect_;
-		}
+        const node_rect& rect() const
+        {
+            return rect_;
+        }
 
-		void clear()
-		{
-			root_.clear();
-		}
+        void clear()
+        {
+            root_.clear();
+        }
 
         void erase(objectid_t uid)
         {
@@ -357,10 +357,10 @@ namespace math
             }
         }
 
-	private:
-		const node_rect rect_;
-		quad_tree_node root_;
+    private:
+        const node_rect rect_;
+        quad_tree_node root_;
         std::unordered_map<int64_t, quad_tree_node*> items_;
-	};
+    };
 }
 
