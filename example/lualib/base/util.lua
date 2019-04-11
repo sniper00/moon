1c1875f35cae5d51e77e298a91dd966a1470670a
sniper00 <hanyongtao@live.com>
2019-04-11 16:11:30 +0800

    dump rename -> print_r

--- example/lualib/base/util.lua
+++ example/lualib/base/util.lua
@@ -1,5 +1,16 @@
 
 local math_floor = math.floor
+local string_format     = string.format
+local string_len        = string.len
+local debug_traceback   = debug.traceback
+local string_rep        = string.rep
+local string_split      = string.split
+local string_trim       = string.trim
+local tsort             = table.sort
+
+local tonumber = tonumber
+local tostring = tostring
+local type = type
 
 function checknumber(value, base)
     return tonumber(value, base) or 0
@@ -28,5 +39,67 @@ function get_script_path()
     local path = info.source
     path = string.sub(path, 2, -1)
     return string.match(path, "^.*[/\\]")
+end
+
+local function _dump_value(v)
+    if type(v) == "string" then
+        v = "\"" .. v .. "\""
+    end
+    return tostring(v)
+end
+
+function print_r(value, desciption, nesting, _print)
+    if type(nesting) ~= "number" then nesting = 10 end
+    _print = _print or print
+
+    local lookup = {}
+    local result = {}
+    local traceback = string_split(debug_traceback("", 2), "\n")
+    table.insert(result,"\ndump from: " .. string_trim(traceback[2]).."\n")
+
+    local function _dump(_value, _desciption, _indent, nest, keylen)
+        _desciption = _desciption or "<var>"
+        local spc = ""
+        if type(keylen) == "number" then
+            spc = string_rep(" ", keylen - string_len(_dump_value(_desciption)))
+        end
+        if type(_value) ~= "table" then
+            result[#result + 1] = string_format("%s%s%s = %s", _indent, _dump_value(_desciption),
+            spc, _dump_value(_value))
+        elseif lookup[tostring(_value)] then
+            result[#result + 1] = string_format("%s%s%s = *REF*", _indent, _dump_value(_desciption), spc)
+        else
+            lookup[tostring(_value)] = true
+            if nest > nesting then
+                result[#result + 1] = string_format("%s%s = *MAX NESTING*", _indent, _dump_value(_desciption))
+            else
+                result[#result + 1] = string_format("%s%s = {", _indent, _dump_value(_desciption))
+                local indent2 = _indent .. "    "
+                local keys = {}
+                keylen = 0
+                local values = {}
+                for k, v in pairs(_value) do
+                    keys[#keys + 1] = k
+                    local vk = _dump_value(k)
+                    local vkl = string_len(vk)
+                    if vkl > keylen then keylen = vkl end
+                    values[k] = v
+                end
+                tsort(keys, function(a, b)
+                    if type(a) == "number" and type(b) == "number" then
+                        return a < b
+                    else
+                        return tostring(a) < tostring(b)
+                    end
+                end)
+                for _, k in ipairs(keys) do
+                    _dump(values[k], k, indent2, nest + 1, keylen)
+                end
+                result[#result + 1] = string_format("%s}", _indent)
+            end
+        end
+    end
+    _dump(value, desciption, "- ", 1)
+    _print(table.concat(result,"\n"))
 end
 
--- example/lualib/moon/log.lua
+++ example/lualib/moon/log.lua
@@ -1,8 +1,6 @@
 local c = require("moon_core")
 local string_format     = string.format
-local string_len        = string.len
 local debug_traceback   = debug.traceback
-local string_rep        = string.rep
 local string_split      = string.split
 local string_trim       = string.trim
 local M = {}
@@ -38,76 +36,6 @@ local function do_log(bcle,level,fmt, ... )
     local str = string_format(fmt, ...)
     local traceback = string_split(debug_traceback("", 3), "\n")
     logV(bcle, level, table.concat({str,"(",string_trim(traceback[2]),")"}))
-end
-
-local print = print
-
-local function _dump_value(v)
-    if type(v) == "string" then
-        v = "\"" .. v .. "\""
-    end
-    return tostring(v)
-end
-
-function M.dump(value, desciption, nesting, _print)
-    if type(nesting) ~= "number" then nesting = 3 end
-    _print = _print or print
-
-    local dumpstr
-    local lookup = {}
-    local result = {}
-    local traceback = string_split(debug_traceback("", 2), "\n")
-    dumpstr = "\ndump from: " .. string_trim(traceback[2]).."\n"
-
-    local function _dump(_value, _desciption, _indent, nest, keylen)
-        _desciption = _desciption or "<var>"
-        local spc = ""
-        if type(keylen) == "number" then
-            spc = string_rep(" ", keylen - string_len(_dump_value(_desciption)))
-        end
-        if type(_value) ~= "table" then
-            result[#result + 1] = string_format("%s%s%s = %s", _indent, _dump_value(_desciption),
-            spc, _dump_value(_value))
-        elseif lookup[tostring(_value)] then
-            result[#result + 1] = string_format("%s%s%s = *REF*", _indent, _dump_value(_desciption), spc)
-        else
-            lookup[tostring(_value)] = true
-            if nest > nesting then
-                result[#result + 1] = string_format("%s%s = *MAX NESTING*", _indent, _dump_value(_desciption))
-            else
-                result[#result + 1] = string_format("%s%s = {", _indent, _dump_value(_desciption))
-                local indent2 = _indent .. "    "
-                local keys = {}
-                keylen = 0
-                local values = {}
-                for k, v in pairs(_value) do
-                    keys[#keys + 1] = k
-                    local vk = _dump_value(k)
-                    local vkl = string_len(vk)
-                    if vkl > keylen then keylen = vkl end
-                    values[k] = v
-                end
-                table.sort(keys, function(a, b)
-                    if type(a) == "number" and type(b) == "number" then
-                        return a < b
-                    else
-                        return tostring(a) < tostring(b)
-                    end
-                end)
-                for _, k in ipairs(keys) do
-                    _dump(values[k], k, indent2, nest + 1, keylen)
-                end
-                result[#result + 1] = string_format("%s}", _indent)
-            end
-        end
-    end
-    _dump(value, desciption, "- ", 1)
-
-    for _, line in ipairs(result) do
-        dumpstr = dumpstr..tostring(line).."\n"
-    end
-
-    _print(dumpstr)
 end
 
 function M.error(fmt, ...)
