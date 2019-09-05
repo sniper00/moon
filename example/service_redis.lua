e8dde9b99a32d47fa9a4aa878c2305cfc4c5647a
sniper00 <hanyongtao@live.com>
2019-09-05 10:30:04 +0800

    add redis service example

---
+++ example/service_redis.lua
@@ -0,0 +1,115 @@
+local moon = require("moon")
+local socket = require("moon.socket")
+local queue = require("moon.queue")
+local redis = require("moon.db.redis")
+
+local tbinsert = table.insert
+local tbremove = table.remove
+local tbunpack = table.unpack
+
+local conf = ...
+
+local function connect(db_conf, auto)
+    local db, err
+    repeat
+        db, err = redis.connect(db_conf)
+        if not db then
+            print("redis connect failed ", err)
+        end
+
+        if not db and auto then
+            moon.co_wait(100)
+        end
+    until(not auto or db)
+    return db, err
+end
+
+local function exec_one(db, sender, sessionid, args , auto)
+    repeat
+        if not db then
+            local _db,err = connect(conf, auto)
+            if not _db then
+                moon.response("lua", sender, sessionid, nil, err)
+                return
+            end
+            db = _db
+        end
+
+        local cmd = args[1]
+        local res, err = db[cmd](db, tbunpack(args,2))
+        if redis.socket_error == res then
+            db = nil
+            print("redis socket error",res, err)
+            if auto then
+                moon.co_wait(100)
+            end
+        else
+            moon.response("lua", sender, sessionid, res)
+            return db
+        end
+    until false
+end
+
+local db_num = conf.connection_num
+local balance = 1
+local connection_pool = {}
+
+for _=1,db_num do
+    tbinsert(connection_pool,{queue = queue.new()})
+end
+
+local function call(sender, sessionid, ...)
+    local args = {...}
+
+    local ctx = connection_pool[balance]
+
+    balance = balance + 1
+    if balance > db_num then
+        balance = 1
+    end
+
+    ctx.queue:run(function()
+        ctx.db = exec_one(ctx.db, sender, sessionid, args)
+    end)
+end
+
+local send_queue = {}
+local sending = false
+local db
+local function send(...)
+    tbinsert(send_queue, {...})
+    if sending then
+        return
+    end
+
+    moon.async(function()
+        sending = true
+        while #send_queue >0 do
+            local req = send_queue[1]
+            db = exec_one(db, 0, 0, req, true)
+            if db then
+                tbremove(send_queue,1)
+            end
+        end
+        sending = false
+    end)
+end
+
+local fd = socket.sync_connect(conf.host,conf.port,moon.PTYPE_TEXT)
+assert(fd, "connect db redis failed")
+
+socket.close(fd)
+
+moon.dispatch('lua',function(msg,p)
+    if #msg:header() >0 then
+        send(p.unpack(msg))
+    else
+        call(msg:sender(), msg:sessionid(), p.unpack(msg))
+    end
+end)
+
+moon.exit(function()
+    moon.async(function()
+        moon.co_wait_exit()
+    end)
+end)
