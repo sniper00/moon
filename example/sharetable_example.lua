9c12b353237cf2972b5b7ab0e04d67d5c49a7160
sniper00 <hanyongtao@live.com>
2019-11-29 10:56:28 +0800

    add sharetable example

---
+++ example/sharetable_example.lua
@@ -0,0 +1,22 @@
+local moon = require("moon")
+local sharetable = require("service/sharetable")
+
+local data
+
+local file = "sharetable_data.lua"
+
+moon.start(function()
+    moon.async(function()
+        sharetable.loadfile(file)
+        data = sharetable.query(file)
+        print(data)
+        print_r(data)
+
+        moon.sleep(3000)
+
+        sharetable.loadfile(file)
+        sharetable.update(file)
+        print(data)
+        print_r(data)
+    end)
+end)
