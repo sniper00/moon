ab494b221a478eb25dc318a506d34bb2e4d1ca1e
bruce <hanyongtao@live.com>
2020-03-12 20:28:40 +0800

    update

---
+++ example/example_log.lua
@@ -0,0 +1,34 @@
+local log = require("moon.log")
+
+do
+    local ok = pcall(log.throw, "log throw")
+    if not ok then
+        print("log.throw")
+    end
+end
+
+do
+    log.error("log error %s","balaba")
+end
+
+do
+    log.warn("log warn")
+end
+
+do
+    log.debug("log debug")
+end
+
+do
+    log.info("log info")
+end
+
+log.LOG_LEVEL = log.LOG_WARN
+log.info("this line will not print")
+log.debug("this line will not print")
+log.warn("this line will  print")
+log.error("this line will  print")
+
+log.LOG_LEVEL = log.LOG_DEBUG
+log.DEBUG = false
+log.debug("this line will not print or log file")
--- example/sharetable_data.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-    a = 1,
-    b = "hello",
-    c = {
-        1,2,3,4,5
-    }
-}
-
-return M
