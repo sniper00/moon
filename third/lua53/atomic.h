ec0c7b168c45ad578089b89a7349447ba163552e
bruce <hanyongtao@live.com>
2020-03-12 20:25:27 +0800

    commit

--- third/lua53/atomic.h
+++ third/lua53/atomic.h
@@ -2,27 +2,16 @@
 #define _ATOMIC_H___
 
 #ifdef _MSC_VER
-
-#include <windows.h>
-#define inline __inline
+#include <Windows.h>
 
 #define ATOM_CAS(ptr, oval, nval) (InterlockedCompareExchange((LONG volatile *)ptr, nval, oval) == oval)
 #define ATOM_CAS_POINTER(ptr, oval, nval) (InterlockedCompareExchangePointer((PVOID volatile *)ptr, nval, oval) == oval)
-#define ATOM_INC(ptr) InterlockedIncrement((LONG volatile *)ptr)
-#define ATOM_FINC(ptr)  (InterlockedIncrement((LONG volatile *)ptr)-1)
-#define ATOM_DEC(ptr) InterlockedDecrement((LONG volatile *)ptr)
-#define ATOM_ADD(ptr,n) InterlockedExchangeAdd((LONG volatile *)ptr, n)
-#define ATOM_SUB(ptr,n) InterlockedExchangeAdd((LONG volatile *)ptr, -n)
-#define ATOM_AND(ptr,n)  InterlockedExchange((LONG volatile *)ptr,n)
-#define atom_sync() MemoryBarrier()
-#define atom_spinlock(ptr) while (InterlockedExchange((LONG volatile *)ptr , 1)) {};
-#define atom_spintrylock(ptr) (InterlockedExchange((LONG volatile *)ptr , 1) == 0)
-#define atom_spinunlock(ptr) InterlockedExchange((LONG volatile *)ptr, 0);
-
-struct spinlock {
-	LONG lock;
-};
-
+#define ATOM_INC(ptr) (InterlockedIncrement64((LONG64 volatile *)ptr))
+#define ATOM_FINC(ptr)  (InterlockedIncrement64((LONG64 volatile *)ptr)-1)
+#define ATOM_DEC(ptr) (InterlockedDecrement64((LONG64 volatile *)ptr))
+#define ATOM_ADD(ptr,n) (InterlockedAdd64((LONG64 volatile *)ptr, n))
+#define ATOM_SUB(ptr,n) (InterlockedAdd64((LONG64 volatile *)ptr, -n))
+#define ATOM_AND(ptr,n)  (InterlockedAnd64((LONG64 volatile *)ptr,n))
 #else
 
 #define ATOM_CAS(ptr, oval, nval) __sync_bool_compare_and_swap(ptr, oval, nval)
@@ -30,18 +19,11 @@ struct spinlock {
 #define ATOM_INC(ptr) __sync_add_and_fetch(ptr, 1)
 #define ATOM_FINC(ptr) __sync_fetch_and_add(ptr, 1)
 #define ATOM_DEC(ptr) __sync_sub_and_fetch(ptr, 1)
+#define ATOM_FDEC(ptr) __sync_fetch_and_sub(ptr, 1)
 #define ATOM_ADD(ptr,n) __sync_add_and_fetch(ptr, n)
 #define ATOM_SUB(ptr,n) __sync_sub_and_fetch(ptr, n)
 #define ATOM_AND(ptr,n) __sync_and_and_fetch(ptr, n)
 
-#define atom_sync() __sync_synchronize()
-#define atom_spinlock(ptr) while (__sync_lock_test_and_set(ptr,1)) {}
-#define atom_spintrylock(ptr) (__sync_lock_test_and_set(ptr,1) == 0)
-#define atom_spinunlock(ptr) __sync_lock_release(ptr)
-
-struct spinlock {
-	int lock;
-};
 #endif
 
 #endif
