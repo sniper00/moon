94494e3ea355188ea30b91587b73de834be625e8
bruce <hanyongtao@live.com>
2020-06-11 15:13:12 +0800

    更新示例

--- example/example_timer.lua
+++ example/example_timer.lua
@@ -11,6 +11,16 @@ moon.start(function()
         end
     end)
 
+    local count2 = 1
+    moon.repeated(1000,-1,function(timerid)
+        print("infinite timer tick",count2)
+        count2 = count2 + 1
+        if count2 >5 then
+            moon.remove_timer(timerid)
+            print("remove infinite timer ")
+        end
+    end)
+
     moon.async(function()
         print("coroutine timer start")
         moon.sleep(1000)
