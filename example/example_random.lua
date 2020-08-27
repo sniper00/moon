805f675795cbc53dde761cff962e682c23129277
bruce <hanyongtao@live.com>
2020-08-27 17:53:39 +0800

    commit

---
+++ example/example_random.lua
@@ -0,0 +1,43 @@
+local moon = require("moon")
+---@type random
+local random = require("random")
+
+
+for i=1,100 do
+    -------------------------[min,max]
+    print("rand_range", random.rand_range(1,10))
+end
+
+for i=1,100 do
+    -------------------------[min,max]
+    print("rand_range_some", table.concat(random.rand_range_some(1,10,2)," "))
+end
+
+local res = {}
+for i=1,10000 do
+	local v = random.rand_weight({1,2,3,4,5},{100,200,300,400,500})
+	if res[v] then
+		res[v] = res[v]+1
+	else
+		res[v] = 1
+	end
+end
+
+print_r(res)
+
+print_r(random.rand_range_some(1,100,10))
+
+res={}
+for i=1,10000 do
+	local vv = random.rand_weight_some({1,2,3,4,5},{100,200,300,400,500},5)
+	for _,v in ipairs(vv) do
+		local v = random.rand_weight({1,2,3,4,5},{100,200,300,400,500})
+		if res[v] then
+			res[v] = res[v]+1
+		else
+			res[v] = 1
+		end
+	end
+end
+
+print_r(res)
