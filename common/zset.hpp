dc885334b09904183144c8272183543c07a378b7
bruce <hanyongtao@live.com>
2021-01-18 18:12:15 +0800

    add simple zset lua lib for ranklist

---
+++ common/zset.hpp
@@ -0,0 +1,190 @@
+#pragma once
+#include <cstdint>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+namespace moon
+{
+    class zset
+    {
+        struct context
+        {
+            int64_t key = 0;
+            int64_t score = 0;
+            int64_t timestamp = 0;
+            uint32_t rank = 0;
+        };
+
+        struct compare
+        {
+            bool operator()(context* a, context* b) const
+            {
+                if (a->score == b->score)
+                {
+                    if (a->timestamp == b->timestamp)
+                    {
+                        return a->key < b->key;
+                    }
+                    return a->timestamp < b->timestamp;
+                }
+                return a->score > b->score;
+            }
+        };
+    public:
+        using container_type = std::set<context*, compare>;
+        using iterator = typename container_type::iterator;
+        using const_iterator = typename container_type::const_iterator;
+        using pointer = typename container_type::pointer;
+        using const_pointer = typename container_type::const_pointer;
+
+        static constexpr size_t JUMP_STEP = 100;
+
+        zset(size_t max_count = std::numeric_limits<size_t>::max())
+            :max_count_(max_count)
+        {
+        }
+
+        void update(int64_t key, int64_t score, int64_t timestamp)
+        {
+            if (max_count_ == 0)
+            {
+                return;
+            }
+
+            auto iter = index_.find(key);
+            if (index_.size() == max_count_ && iter == index_.end() && (*(order_.rbegin()))->score > score)
+            {
+                return;
+            }
+
+            if (iter == index_.end())
+            {
+                iter = index_.emplace(key, context{ key, score, timestamp }).first;
+            }
+            else
+            {
+                order_.erase(&iter->second);
+            }
+
+            iter->second.score = score;
+            iter->second.timestamp = timestamp;
+
+            order_.emplace(&iter->second);
+
+            if (order_.size() > max_count_)
+            {
+                auto v = (*order_.rbegin());
+                order_.erase(v);
+                index_.erase(v->key);
+            }
+
+            ordered_ = false;
+        }
+
+        uint32_t rank(int64_t key)
+        {
+            sort();
+            auto iter = index_.find(key);
+            if (iter != index_.end())
+            {
+                return iter->second.rank;
+            }
+            return 0;
+        }
+
+        void sort()
+        {
+            if (!ordered_)
+            {
+                jump_.clear();
+                uint32_t n = 0;
+                for (auto iter = order_.begin(); iter != order_.end(); ++iter)
+                {
+                    if (n % JUMP_STEP == 0)
+                    {
+                        jump_.emplace_back(iter);
+                    }
+                    (*iter)->rank = ++n;
+                }
+                ordered_ = true;
+            }
+        }
+
+        int64_t score(int64_t key)
+        {
+            auto iter = index_.find(key);
+            if (iter != index_.end())
+            {
+                return iter->second.score;
+            }
+            return 0;
+        }
+
+        const_iterator start(uint32_t nrank) const
+        {
+            size_t idx = nrank / JUMP_STEP;
+            if (idx >= jump_.size())
+            {
+                return  order_.end();
+            }
+
+            for (auto iter = jump_[idx]; iter != order_.end(); ++iter)
+            {
+                if ((*iter)->rank == nrank)
+                {
+                    return iter;
+                }
+            }
+            return  order_.end();
+        }
+
+        const_iterator begin() const
+        {
+            return order_.begin();
+        }
+
+        const_iterator end() const
+        {
+            return  order_.end();
+        }
+
+        size_t size() const
+        {
+            return order_.size();
+        }
+
+        bool has(int64_t key) const
+        {
+            return (index_.find(key) != index_.end());
+        }
+
+        void clear()
+        {
+            jump_.clear();
+            order_.clear();
+            index_.clear();
+        }
+
+        size_t erase(int64_t key)
+        {
+            auto iter = index_.find(key);
+            if (iter != index_.end())
+            {
+                order_.erase(&iter->second);
+                ordered_ = false;
+                index_.erase(iter);
+                return 1;
+            }
+            return 0;
+        }
+    private:
+        bool ordered_ = true;
+        size_t max_count_;
+        std::vector<const_iterator> jump_;
+        std::set<context*, compare> order_;
+        std::unordered_map<int64_t, context> index_;
+    };
+}
+
+
---
+++ moon-src/luabind/lua_zset.cpp
@@ -0,0 +1,210 @@
+#include "common/zset.hpp"
+#include "lua.hpp"
+
+#define METANAME "lzet"
+
+struct zset_proxy
+{
+    moon::zset* zset;
+};
+
+static int lupdate(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+
+    int64_t key = (int64_t)luaL_checkinteger(L, 2);
+    int64_t score = (int64_t)luaL_checkinteger(L, 3);
+    int64_t timestamp = (int64_t)luaL_checkinteger(L, 4);
+    proxy->zset->update(key, score, timestamp);
+    return 0;
+}
+
+static int lsort(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    proxy->zset->sort();
+    return 0;
+}
+
+static int lrank(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    int64_t key = (int64_t)luaL_checkinteger(L, 2);
+    lua_pushinteger(L, proxy->zset->rank(key));
+    return 1;
+}
+
+static int lkey(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    uint32_t rank = (uint32_t)luaL_checkinteger(L, 2);
+    proxy->zset->sort();
+    auto iter = proxy->zset->start(rank);
+    if (iter == proxy->zset->end())
+    {
+        return 0;
+    }
+    lua_pushinteger(L, (*iter)->key);
+    return 1;
+}
+
+static int lscore(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    int64_t key = (int64_t)luaL_checkinteger(L, 2);
+    lua_pushinteger(L, proxy->zset->score(key));
+    return 1;
+}
+
+static int lhas(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    int64_t key = (int64_t)luaL_checkinteger(L, 2);
+    lua_pushboolean(L, proxy->zset->has(key)?1:0);
+    return 1;
+}
+
+static int lsize(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    lua_pushinteger(L, proxy->zset->size());
+    return 1;
+}
+
+static int lclear(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    proxy->zset->clear();
+    return 0;
+}
+
+static int lerase(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    int64_t key = (int64_t)luaL_checkinteger(L, 2);
+    lua_pushinteger(L, proxy->zset->erase(key));
+    return 1;
+}
+
+static int lrange(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (nullptr == proxy || nullptr == proxy->zset)
+    {
+        return luaL_error(L, "invalid lua-zset pointer");
+    }
+    uint32_t start = (uint32_t)luaL_checkinteger(L, 2);
+    uint32_t end = (uint32_t)luaL_checkinteger(L, 3);
+
+    auto count = end - start + 1;
+    if (end < start || count == 0)
+    {
+        return 0;
+    }
+    proxy->zset->sort();
+    auto iter = proxy->zset->start(start);
+    if (iter == proxy->zset->end())
+    {
+        return 0;
+    }
+    lua_createtable(L, std::min(32U, count), 0);
+    int idx = 1;
+    for (; iter != proxy->zset->end() && count > 0; ++iter)
+    {
+        lua_pushinteger(L, (*iter)->key);
+        --count;
+        lua_rawseti(L, -2, idx++);
+    }
+    return 1;
+};
+
+static int lrelease(lua_State* L)
+{
+    zset_proxy* proxy = (zset_proxy*)lua_touserdata(L, 1);
+    if (proxy && proxy->zset)
+    {
+        delete proxy->zset;
+        proxy->zset = nullptr;
+    }
+    return 0;
+}
+
+static int lcreate(lua_State* L)
+{
+    size_t max_count = (size_t)luaL_checkinteger(L, 1);
+    zset_proxy* proxy = (zset_proxy*)lua_newuserdatauv(L, sizeof(zset_proxy), 0);
+    proxy->zset = new moon::zset(max_count);
+    if (luaL_newmetatable(L, METANAME))//mt
+    {
+        luaL_Reg l[] = {
+            { "update", lupdate },
+            { "sort",lsort },
+            { "has", lhas},
+            { "rank", lrank},
+            { "key", lkey},
+            { "score", lscore},
+            { "range", lrange},
+            { "clear", lclear},
+            { "size", lsize},
+            { "erase", lerase},
+            { NULL,NULL }
+        };
+        luaL_newlib(L, l); //{}
+        lua_setfield(L, -2, "__index");//mt[__index] = {}
+        lua_pushcfunction(L, lrelease);
+        lua_setfield(L, -2, "__gc");//mt[__gc] = lrelease
+    }
+    lua_setmetatable(L, -2);// set userdata metatable
+    return 1;
+}
+
+extern "C"
+{
+    int LUAMOD_API luaopen_zset(lua_State* L)
+    {
+        luaL_Reg l[] = {
+            {"new",lcreate},
+            {"release",lrelease },
+            {NULL,NULL}
+        };
+        luaL_checkversion(L);
+        luaL_newlib(L, l);
+        return 1;
+    }
+}
