4d6baa1f43044f4b042cea5d53321a99f2430124
bruce <hanyongtao@live.com>
2021-04-10 17:52:12 +0800

    state_deleter and traceback

--- common/lua_utility.hpp
+++ common/lua_utility.hpp
@@ -188,4 +188,20 @@ namespace moon
             return const_iterator{ this,  size() };
         }
     };
+
+    struct state_deleter {
+        void operator()(lua_State* L) const {
+            lua_close(L);
+        }
+    };
+
+    inline int traceback(lua_State* L) {
+        const char* msg = lua_tostring(L, 1);
+        if (msg)
+            luaL_traceback(L, L, msg, 1);
+        else {
+            lua_pushliteral(L, "(no error message)");
+        }
+        return 1;
+    }
 }
