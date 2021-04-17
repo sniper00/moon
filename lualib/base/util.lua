8dff05cf1f96512783310d3147b9df8f80e73750
bruce <hanyongtao@live.com>
2021-04-17 18:47:48 +0800

    hash queue

--- lualib/base/util.lua
+++ lualib/base/util.lua
@@ -116,3 +116,49 @@ function print_r(value, desciption, nesting, _print)
     _print(tbconcat(result,"\n"))
 end
 
+---create a queue based hash
+function make_queue()
+    return { h = 1, t = 0 }
+end
+
+function queue_push(q, v)
+    local t = q.t + 1
+	q.t = t
+	q[t] = v
+end
+
+function queue_front(q)
+    if q.h > q.t then
+        -- queue is empty
+        q.h = 1
+        q.t = 0
+        return
+    end
+    local h = q.h
+    return q[h]
+end
+
+function queue_pop(q)
+    if q.h > q.t then
+        -- queue is empty
+        q.h = 1
+        q.t = 0
+        return
+    end
+    -- pop queue
+    local h = q.h
+    local v = q[h]
+    q[h] = nil
+    q.h = h + 1
+    return v
+end
+
+function queue_size(q)
+    local size = 0
+    for k,_ in pairs(q) do
+        if k~="h" and k~="t" then
+            size = size + 1
+        end
+    end
+    return size
+end
