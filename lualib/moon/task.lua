b1d1e85dfdd4b691f880a7bad8411dd80f9883ff
bruce <hanyongtao@live.com>
2021-06-19 18:07:53 +0800

    delay resume coroutine

--- lualib/moon/task.lua
+++ lualib/moon/task.lua
@@ -17,6 +17,9 @@ local _M = {}
 function _M.wait_all(fnlist)
     local n = #fnlist
     local res = {}
+    if n == 0 then
+        return res
+    end
     local co = corunning()
     for i,fn in ipairs(fnlist) do
         moon.async(function ()
@@ -25,18 +28,14 @@ function _M.wait_all(fnlist)
                 table.remove(res[i], 1)
             end
             n=n-1
-            if costatus(co) == "suspended" then
-                coresume(co)
+            if n==0 then
+                if costatus(co) == "suspended" then
+                    moon.wakeup(co)
+                end
             end
         end)
     end
-
-    while true do
-        if n==0 then
-            break
-        end
-        coyield()
-    end
+    coyield()
     return res
 end
 
