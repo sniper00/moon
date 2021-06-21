a799937e7ff5f30cc367969236e6685828109d08
bruce <hanyongtao@live.com>
2021-06-21 11:55:16 +0800

    maybe has error

--- lualib/moon/task.lua
+++ lualib/moon/task.lua
@@ -1,7 +1,6 @@
 local moon = require("moon")
 local corunning = coroutine.running
 local costatus = coroutine.status
-local coresume = coroutine.resume
 local coyield = coroutine.yield
 
 local traceback = debug.traceback
@@ -21,20 +20,23 @@ function _M.wait_all(fnlist)
         return res
     end
     local co = corunning()
-    for i,fn in ipairs(fnlist) do
-        moon.async(function ()
-            res[i] = {xpcall(fn, traceback)}
-            if res[i][1] then
-                table.remove(res[i], 1)
-            end
-            n=n-1
-            if n==0 then
-                if costatus(co) == "suspended" then
-                    moon.wakeup(co)
+    moon.timeout(0, function()
+        for i,fn in ipairs(fnlist) do
+            moon.async(function ()
+                local one = {xpcall(fn, traceback)}
+                if one[1] then
+                    table.remove(one, 1)
                 end
-            end
-        end)
-    end
+                res[i] = one
+                n=n-1
+                if n==0 then
+                    if costatus(co) == "suspended" then
+                        moon.wakeup(co)
+                    end
+                end
+            end)
+        end
+    end)
     coyield()
     return res
 end
