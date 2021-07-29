526b55fe826254ad265cf288676741854b0bd88d
bruce <hanyongtao@live.com>
2021-07-29 16:25:59 +0800

    update example

--- example/create_service_benchmark.lua
+++ example/create_service_benchmark.lua
@@ -3,61 +3,24 @@ local moon = require("moon")
 local conf = ...
 
 if conf and conf.slave then
-    local command = {}
-
-    command.QUIT = function ()
-        print("recv quit cmd, bye bye")
-        moon.quit()
-    end
-
-    local function docmd(sender,header,...)
-    -- body
-        local f = command[header]
-        if f then
-            f(sender,...)
-        else
-            error(string.format("Unknown command %s", tostring(header)))
-        end
-    end
-
-    print("conf:", conf.message)
-
-    moon.dispatch('lua',function(msg,unpack)
-        local sender, sz, len = moon.decode(msg,"SC")
-        docmd(sender, unpack(sz, len))
-    end)
-
     if conf.auto_quit then
         print("auto quit, bye bye")
         -- 使服务退出
-        moon.quit()
+        moon.timeout(10, function()
+            moon.quit()
+        end)
     end
 else
-
     moon.async(function()
-        for i=1,8 do
-        -- 动态创建服务，获得服务ID，方便用来通信
-        local serviceid =  moon.new_service("lua", {
-            name = "create_service",
-            file = "example_create_service.lua",
-            slave = true,
-            message = "Hello create_service2"
-        }, false, i)
-    end
-
-
-    while true do
-        -- 动态创建服务, 配置同时可以用来传递一些信息
-        moon.new_service("lua", {
-            name = "create_service",
-            file = "example_create_service.lua",
-            message = "Hello create_service",
-            slave = true,
-            auto_quit = true
-        })
-    end
-
-
+        while true do
+            moon.new_service("lua", {
+                name = "create_service",
+                file = "create_service_benchmark.lua",
+                message = "Hello create_service",
+                slave = true,
+                auto_quit = true
+            })
+        end
     end)
 end
 
