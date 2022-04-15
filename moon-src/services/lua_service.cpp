dafeb44d07307d15a3a5590b13cecbacb9301780
bruce <hanyongtao@live.com>
2022-04-15 15:54:29 +0800

    correct bind lua callback

--- moon-src/services/lua_service.cpp
+++ moon-src/services/lua_service.cpp
@@ -122,7 +122,10 @@ bool lua_service::init(const moon::service_conf& conf)
     lua_gc(L, LUA_GCSTOP, 0);
     lua_gc(L, LUA_GCGEN, 0, 0);
 
-    std::string initialize_string = server_->get_env("PATH");
+    std::string initialize_string;
+    auto lua_path = server_->get_env("PATH");
+    if (lua_path)
+        initialize_string.append(*lua_path);
     initialize_string.append(conf.params);
 
     lua_pushcfunction(L, protect_init);
@@ -150,24 +153,33 @@ bool lua_service::init(const moon::service_conf& conf)
     return ok_;
 }
 
+//https://blog.codingnow.com/2022/04/lua_binding_callback.html#more
+int lua_service::set_callback(lua_State* L) {
+    lua_service* S = lua_service::get(L);
+    luaL_checktype(L, 1, LUA_TFUNCTION);
+    lua_settop(L, 1);
+    callback_context* cb_ctx = (callback_context*)lua_newuserdata(L, sizeof(callback_context)); // L [-0, +1]
+    cb_ctx->L = lua_newthread(L);                                                               // L [-0, +1]
+    lua_pushcfunction(cb_ctx->L, traceback);                                                    // cb_ctx->L [-0, +1]
+    lua_setuservalue(L, -2);                                // L [-1, +0]  associate cb_ctx->L to the userdata
+    lua_setfield(L, LUA_REGISTRYINDEX, "callback_context"); // L [-1, +0] ref userdata(cb_ctx), avoid free by GC
+    lua_xmove(L, cb_ctx->L, 1); // L [-1, +0], cb_ctx->L [-0, +1] move callback-function to cb_ctx->L, cb_ctx->L's top=2
+    S->cb_ctx = cb_ctx;
+    return 0;
+}
+
 void lua_service::dispatch(message* msg)
 {
     if (!ok())
         return;
-    lua_State* L = lua_.get();
+    if (cb_ctx == nullptr) {
+        logger()->logstring(true, moon::LogLevel::Error, "callback not init", id());
+        return;
+    }
+    lua_State* L = cb_ctx->L;
     try
     {
         int trace = 1;
-        int top = lua_gettop(L);
-        if (top == 0)
-        {
-            lua_pushcfunction(L, traceback);
-            lua_rawgetp(L, LUA_REGISTRYINDEX, this);
-        }
-        else
-        {
-            assert(top == 2);
-        }
         lua_pushvalue(L, 2);
 
         lua_pushlightuserdata(L, msg);
