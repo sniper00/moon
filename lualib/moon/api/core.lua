27dcfb3b5675152bf446bb252475b9c8d679c4aa
bruce <hanyongtao@live.com>
2022-07-09 22:20:50 +0800

    code intellisense

--- lualib/moon/api/core.lua
+++ lualib/moon/api/core.lua
@@ -17,35 +17,25 @@ local core = {}
 
 ---same as os.clock()
 ---@return number @in seconds
-function core.clock()
-    -- body
-end
+function core.clock() end
 
 ---string's md5.
 ---@param data string
 ---@return string
-function core.md5(data)
-
-end
+function core.md5(data) end
 
 ---convert c-string(char* and size) to lua-string
 ---@param sz cstring_ptr
 ---@param len integer
 ---@return string
-function core.tostring(sz, len)
-
-end
+function core.tostring(sz, len) end
 
 ---@return string @get server info
-function core.server_info()
-
-end
+function core.server_info() end
 
 ---if linked mimalloc, call mimalloc collect
 ---@param force boolean @
-function core.collect(force)
-
-end
+function core.collect(force) end
 
 -- ---create a timer
 -- ---@param interval integer @ms
@@ -55,34 +45,25 @@ end
 -- end
 
 --- print console log
-function core.log(loglv,...)
-
-end
+function core.log(loglv,...) end
 
 --- set log level
 ---@param lv string @DEBUG, INFO, WARN, ERROR
 ---@return integer
-function core.set_loglevel(lv)
-
-end
+function core.set_loglevel(lv) end
 
 --- get log level
 ---@return integer
-function core.get_loglevel()
-end
+function core.get_loglevel() end
 
 --- get this service's cpu cost time
 ---@return integer
-function core.cpu()
-    -- body
-end
+function core.cpu() end
 
 ---make a prefab message
 ---@param data string|buffer_ptr
 ---@return integer @prefab id
-function core.make_prefab(data)
-
-end
+function core.make_prefab(data) end
 
 ---send prefab message
 ---@param receiver integer
@@ -90,9 +71,7 @@ end
 ---@param header string
 ---@param sessionid integer
 ---@param type integer
-function core.send_prefab(receiver, prefabid, header, sessionid, type)
-
-end
+function core.send_prefab(receiver, prefabid, header, sessionid, type) end
 
 -- --- send message from `sender` to `receiver`
 -- ---@param receiver integer
@@ -104,50 +83,34 @@ end
 -- end
 
 --- remove a service
-function core.kill(addr, sessionid)
-
-end
+function core.kill(addr, sessionid) end
 
 --- query **unique** service's address by name
-function core.queryservice(name)
-
-end
+function core.queryservice(name) end
 
 ---@param key string
 ---@param value string
-function core.set_env(key, value)
-
-end
+function core.set_env(key, value) end
 
 ---@param key string
 ---@return string
-function core.get_env(key)
-
-end
+function core.get_env(key) end
 
 ---get worker thread info
 ---@return string
-function core.wsate(workerid)
-
-end
+function core.wsate(workerid) end
 
 --- let server exit: exitcode>=0 will wait all services quit.
 ---@param exitcode integer
-function core.exit(exitcode)
-
-end
+function core.exit(exitcode) end
 
 --- get total services's count()
 ---@return integer
-function core.size()
-    -- body
-end
+function core.size() end
 
 --- adjusts server time(millsecond)
 ---@param milliseconds integer
-function core.adjtime(milliseconds)
-
-end
+function core.adjtime(milliseconds) end
 
 -- --- set lua callback
 -- ---@param fn fun(msg:userdata,ptype:integer)
@@ -157,9 +120,7 @@ end
 
 --- Get server timestamp in milliseconds
 --- @return integer
-function core.now()
-
-end
+function core.now() end
 
 --- get message's field
 ---
@@ -175,79 +136,59 @@ end
 ---@param pattern string
 ---@return ...
 ---@nodiscard
-function core.decode(msg, pattern)
-
-end
+function core.decode(msg, pattern) end
 
 ---clone message, but share buffer field
 ---@param msg message_ptr
 ---@return userdata
-function core.clone(msg)
-
-end
+function core.clone(msg) end
 
 ---release clone message
 ---@param msg message_ptr
-function core.release(msg)
-
-end
+function core.release(msg) end
 
 ---redirect a message to other service
-function core.redirect(msg, header, receiver, mtype, sender, sessionid)
-
-end
+function core.redirect(msg, header, receiver, mtype, sender, sessionid) end
 
 ---get error log count
 ---@return integer
-function core.error_count()
-end
+function core.error_count() end
 
 ---@class asio
 local asio = {}
 
 ---@param host string
 ---@param port integer
-function asio.try_open(host, port)
-
-end
+---@return boolean
+function asio.try_open(host, port) end
 
 ---param protocol moon.PTYPE_SOCKET_TCP, moon.PTYPE_SOCKET_MOON, moon.PTYPE_SOCKET_WS
 ---@param host string
 ---@param port integer
 ---@param protocol integer
-function asio.listen(host, port, protocol)
-
-end
+function asio.listen(host, port, protocol) end
 
 ---send data to fd
 ---@param fd integer
 ---@param data string|buffer_ptr
 ---@param flag? integer
 ---@return boolean
-function asio.write(fd, data, flag)
-
-end
+function asio.write(fd, data, flag) end
 
 ---@param fd integer
 ---@param m message_ptr
 ---@return boolean
-function asio.write_message(fd, m)
-
-end
+function asio.write_message(fd, m) end
 
 --- 设置读操作超时, 默认是0, 永远不会超时。为了处理大量链接的检测,实际超时检测并不严格，误差范围为[t, t+10)
 ---@param fd integer
 ---@param t integer @ seconds
 ---@return boolean
-function asio.settimeout(fd, t)
-
-end
+function asio.settimeout(fd, t) end
 
 ---@param fd integer
 ---@return boolean
-function asio.setnodelay(fd)
-
-end
+function asio.setnodelay(fd) end
 
 ---@alias chunkmode
 ---| 'r' # read.
@@ -259,49 +200,36 @@ end
 ---@param fd integer
 ---@param mode chunkmode
 ---@return boolean
-function asio.set_enable_chunked(fd, mode)
-
-end
+function asio.set_enable_chunked(fd, mode) end
 
 --- set send queue limit
 ---@param fd integer @ fd
 ---@param warnsize integer @ if send queue size > warnsize, print warnning log
 ---@param errorsize integer @ if send queue size > errorsize, print error log and close socket
-function asio.set_send_queue_limit(fd, warnsize, errorsize)
-end
+function asio.set_send_queue_limit(fd, warnsize, errorsize) end
 
 ---@param fd integer
-function asio.close(fd)
-
-end
+function asio.close(fd) end
 
 ---@param fd integer
 ---@return string @ format ip:port
-function asio.getaddress(fd)
-
-end
+function asio.getaddress(fd) end
 
 ---@param fd integer
 ---@param addr string @ addr bytes string. see udp callback function second param or user make_endpoint
 ---@param data string|userdata
 ---@return boolean
-function asio.sendto(fd, addr, data)
-
-end
+function asio.sendto(fd, addr, data) end
 
 ---@param fd integer
 ---@param host string
 ---@param port integer
 ---@return boolean
-function asio.udp_connect(fd, host, port)
-
-end
+function asio.udp_connect(fd, host, port) end
 
 ---@param host string
 ---@param port integer
 ---@return string @addr bytes string
-function asio.make_endpoint(host, port)
-
-end
+function asio.make_endpoint(host, port) end
 
 return core
