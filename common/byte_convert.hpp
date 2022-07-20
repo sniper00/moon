f0c7ef1a828f9998478c68282250ddf1908899ff
bruce <hanyongtao@live.com>
2022-07-20 19:24:53 +0800

    improve code

--- common/byte_convert.hpp
+++ common/byte_convert.hpp
@@ -1,46 +1,50 @@
 #pragma once
 #include <cstdint>
+#include <cstdlib>
+#include <type_traits>
 
-namespace moon
-{
-    //define system endian
+//define system endian
 #define MOON_LITTLEENDIAN
 //#define MOON_BIGENDIAN
 
-    namespace detail
+namespace moon
+{
+    template<typename T>
+    void byte_swap(T& v)
     {
-        template<size_t T>
-        inline void convert(char* val)
-        {
-            std::swap(*val, *(val + T - 1));
-            convert < T - 2 >(val + 1);
-        }
-
-        template<> inline void convert<0>(char*) {}
-        template<> inline void convert<1>(char*) {}             // ignore central byte
-
-        template<typename T>
-        inline void apply(T* val)
-        {
-            convert<sizeof(T)>(reinterpret_cast<char*>(val));
-        }
+        static_assert(std::is_integral_v<T>, "need integer type");
+#ifdef _MSC_VER
+        if constexpr(sizeof(T) == 2)
+            v = static_cast<T>(_byteswap_ushort(static_cast<uint16_t>(v)));
+        else if constexpr(sizeof(T) == 4)
+            v = static_cast<T>(_byteswap_ulong(static_cast<uint32_t>(v)));
+        else if constexpr(sizeof(T) == 8)
+            v = static_cast<T>(_byteswap_uint64(static_cast<uint64_t>(v)));
+#else
+        if constexpr(sizeof(T) == 2)
+            v = static_cast<T>(__builtin_bswap16(static_cast<uint16_t>(v)));
+        else if constexpr(sizeof(T) == 4)
+            v = static_cast<T>(__builtin_bswap32(static_cast<uint32_t>(v)));
+        else if constexpr(sizeof(T) == 8)
+            v = static_cast<T>(__builtin_bswap64(static_cast<uint64_t>(v)));
+#endif
+        else
+            static_assert(sizeof(T)<=8&&(sizeof(T)%2==0), "unsupported");
     }
 
     template<typename T>
     inline void host2net(T& val)
     {
-        static_assert(!std::is_pointer_v<T>, "need value type");
 #ifdef  MOON_LITTLEENDIAN
-        detail::apply<T>(&val);
+        byte_swap(val);
 #endif //  MOON_LITTLEENDIAN	
     }
 
     template<typename T>
     inline void net2host(T& val)
     {
-        static_assert(!std::is_pointer_v<T>, "need value type");
 #ifdef MOON_LITTLEENDIAN
-        detail::apply<T>(&val);
+        byte_swap(val);
 #endif // MOON_LITTLEENDIAN
     }
 }
