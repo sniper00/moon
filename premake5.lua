dda25aceb762d012da9cb2b83574a325adaf0c41
bruce <hanyongtao@live.com>
2022-09-07 16:28:44 +0800

    add mimalloc submodule

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,50 @@
 ---@diagnostic disable: undefined-global
 
+local MOON_ENABLE_MIMALLOC = false
+
+local function detect_visualstudio()
+    local vswhere = os.outputof([[echo %ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe]])
+    if not os.isfile(vswhere) then
+        return false
+    end
+
+    local cmd = string.format('"%s" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath', vswhere)
+    local res = os.outputof(cmd)
+    if not res then
+        --print("Failed", cmd)
+        return false
+    end
+    local BUILDVCTOOLS = res..[[\Common7\IDE\devenv.com]]
+    if not os.isfile(BUILDVCTOOLS) then
+        --print("Failed: Can not find dir", BUILDVCTOOLS)
+        return false
+    end
+    return BUILDVCTOOLS
+end
+
+if MOON_ENABLE_MIMALLOC then
+    os.execute("git submodule init")
+    os.execute("git submodule update")
+    local cwd = os.getcwd()
+    os.mkdir("./third/mimalloc/build")
+    os.chdir("./third/mimalloc/build")
+    os.remove("CMakeCache.txt")
+    os.execute("cmake -D MI_BUILD_STATIC=ON ..")
+    if os.host() == "windows" then
+        local devenv = detect_visualstudio()
+        print("BUILDVCTOOLS", devenv)
+        io.writefile("mimalloc-static.vcxproj",(string.gsub(io.readfile("mimalloc-static.vcxproj"), "MultiThreadedDLL","MultiThreaded")))
+        io.writefile("mimalloc-static.vcxproj",(string.gsub(io.readfile("mimalloc-static.vcxproj"), "MultiThreadedDebugDLL","MultiThreadedDebug")))
+        --print(string.format('"%s" libmimalloc.sln /Project mimalloc-static /Rebuild Release', devenv))
+        os.execute(string.format('"%s" libmimalloc.sln /Project mimalloc-static /Rebuild Release', devenv))
+        assert(os.copyfile("Release/mimalloc-static.lib", "./mimalloc.lib"))
+    else
+        os.execute("make clean")
+        os.execute("make mimalloc-static")
+    end
+    os.chdir(cwd)
+end
+
 workspace "Server"
     configurations { "Debug", "Release" }
     flags{"NoPCH","RelativeLinks"}
@@ -52,7 +97,7 @@ project "moon"
 
     kind "ConsoleApp"
     language "C++"
-    includedirs {"./","./moon-src","./moon-src/core", "./third", "./third/lua","./third/mimalloc/include"}
+    includedirs {"./","./moon-src","./moon-src/core", "./third", "./third/lua"}
     files {"./moon-src/**.h", "./moon-src/**.hpp","./moon-src/**.cpp" }
     links{
         "lua",
@@ -61,13 +106,18 @@ project "moon"
         "pb",
         "sharetable",
         "mongo",
-        -- "mimalloc",
     }
     defines {
         "ASIO_STANDALONE" ,
         "ASIO_NO_DEPRECATED",
-        --"MOON_ENABLE_MIMALLOC"
     }
+
+    if MOON_ENABLE_MIMALLOC then
+        libdirs{"third/mimalloc/build"}
+        links{"mimalloc"}
+        defines {"MOON_ENABLE_MIMALLOC"}
+        includedirs {"./third/mimalloc/include"}
+    end
 
     filter { "system:windows" }
         defines {"_WIN32_WINNT=0x0601"}
@@ -147,8 +197,12 @@ add_lua_module("./lualib-src", "lualib", function()
     defines {
         "ASIO_STANDALONE" ,
         "ASIO_NO_DEPRECATED",
-        --"MOON_ENABLE_MIMALLOC"
     }
+
+    if MOON_ENABLE_MIMALLOC then
+        includedirs {"./third/mimalloc/include"}
+        defines {"MOON_ENABLE_MIMALLOC"}
+    end
 
     ---json
     defines{ "YYJSON_DISABLE_WRITER" }
