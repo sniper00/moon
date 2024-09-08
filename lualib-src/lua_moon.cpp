6614ef3a19566886e30b7546a799a5a155a4500f
bruce <hanyongtao@live.com>
2024-09-08 20:43:26 +0800

    Improve moon.log

--- lualib-src/lua_moon.cpp
+++ lualib-src/lua_moon.cpp
@@ -107,12 +107,37 @@ static int lmoon_log(lua_State* L) {
 
     int n = lua_gettop(L); /* number of arguments */
     for (int i = 2; i <= n; i++) { /* for each argument */
-        size_t l;
-        const char* s = luaL_tolstring(L, i, &l); /* convert it to string */
         if (i > 2) /* not the first element? */
             line.write_back("    ", 4); /* add a tab before it */
-        line.write_back(s, l); /* print it */
-        lua_pop(L, 1); /* pop result */
+        switch (lua_type(L, i)) {
+            case LUA_TNIL:
+                line.write_back("nil", 3);
+                break;
+            case LUA_TNUMBER: {
+                if (lua_isinteger(L, i))
+                    line.write_chars(lua_tointeger(L, i));
+                else
+                    line.write_chars(lua_tonumber(L, i));
+                break;
+            }
+            case LUA_TBOOLEAN: {
+                int v = lua_toboolean(L, i);
+                constexpr std::string_view s[2] = { "true", "false" };
+                line.write_back(s[v].data(), s[v].size());
+                break;
+            }
+            case LUA_TSTRING: {
+                size_t sz = 0;
+                const char* str = lua_tolstring(L, i, &sz);
+                line.write_back(str, sz);
+                break;
+            }
+            default:
+                size_t l;
+                const char* s = luaL_tolstring(L, i, &l); /* convert it to string */
+                line.write_back(s, l); /* print it */
+                lua_pop(L, 1); /* pop result */
+        }
     }
 
     if (lua_Debug ar; lua_getstack(L, 2, &ar) && lua_getinfo(L, "Sl", &ar)) {
